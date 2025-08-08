756f4ef328d107dcdb2866886add6d5403fea352
sabin <sashre@utu.fi>
2025-08-08 13:46:30 +0300

    Initial setup, added components: transaction, saving, balance

---
+++ src/app/components/saving/saving.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-saving',
+  imports: [],
+  templateUrl: './saving.component.html',
+  styleUrl: './saving.component.css'
+})
+export class SavingComponent {
+
+}
